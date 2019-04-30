e8c216bc5bb3045ed646a9c139659ae215d5fb5e
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-04-30 12:35:57 -0500

    created the label, textfield, and slider

--- HammingDistGUI.java
+++ HammingDistGUI.java
@@ -2,7 +2,10 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 public class HammingDistGUI {
 	
@@ -11,12 +14,29 @@ public class HammingDistGUI {
 		JPanel leftSide = new JPanel();
 		JPanel rightSide = new JPanel();
 		
+		JPanel leftContents = new JPanel(new GridLayout(8,1));
+		
+		//add enterHammingDist label and textbox
+		JPanel row1 = new JPanel(new GridLayout(1, 2));
+		JLabel enterHammDist = new JLabel("Enter Hamming Dist:");
+		JTextField inputDist = new JTextField();
+		//TODO have textfield correspond with slider in row 2
+		row1.add(enterHammDist);
+		row1.add(inputDist);
+		leftContents.add(row1);
+		
+		//add slider for row2
+		JSlider row2 = new JSlider(1, 4, 1);
+		row2.setMinorTickSpacing(1);
+	    row2.setMajorTickSpacing(1);
+		row2.setPaintTicks(true);
+	    row2.setPaintLabels(true);
+		leftContents.add(row2);
 		
 		
 		
 		
-		
-		
+		leftSide.add(leftContents);
 		
 		HammingDist.setLayout(new GridLayout(1, 2));
 		HammingDist.add(leftSide);
